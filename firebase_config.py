136db392a4a0259cb27b65c96625ed9910e917d0
M Shree Kamal <161548549+mkamal-23@users.noreply.github.com>
2026-05-05 17:35:15 +0530

    Modify init_firebase to use env variable for credentials
    Update Firebase initialization to use credentials from environment variable instead of a file.

--- firebase_config.py
+++ firebase_config.py
@@ -1,7 +1,11 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import os
+import json
 
 def init_firebase():
-    cred = credentials.Certificate("serviceAccountKey.json")
+    cred_json = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS_JSON")
+    cred_dict = json.loads(cred_json)
+    cred = credentials.Certificate(cred_dict)
     firebase_admin.initialize_app(cred)
     return firestore.client()
